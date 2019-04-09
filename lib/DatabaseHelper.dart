d49765bd415cd19a1156041d6bef57fd5e5ae104
mostafa9696 <mostafamohamed@cis.asu.edu.eg>
2019-04-09 15:37:21 +0200

    Continue paulo udemy course

--- lib/DatabaseHelper.dart
+++ lib/DatabaseHelper.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'dart:io';
 
@@ -8,46 +7,89 @@ import 'package:sqflite/sqlite_api.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DatabaseHelper{
 
-  static final DatabaseHelper _instance = DatabaseHelper.internal(); // internal refer to internal constructor
+class DatabaseHelper {
+
+  static final DatabaseHelper _instance =new DatabaseHelper.internal(); // internal refer to internal constructor
   static Database _db;
   final String tableUser = "userTable";
   final String columnId = "id";
   final String columnUsername = "username";
   final String columnPassword = "password";
 
+  //A factory constructor is permitted to return an existing instance
   factory DatabaseHelper()=> _instance;
-  static DatabaseHelper internal() {}
 
-  Future<Database> get db async{    // getter for db
-    if(_db != null){
+  DatabaseHelper.internal();
+
+  Future<Database> get db async {
+    // getter for db
+    if (_db != null) {
       return _db;
     }
     _db = await initDb();
     return _db;
   }
 
-  Future<Database> initDb() async{
+  Future<Database> initDb() async {
     // get app directory for android or ios
-    Directory directory =await getApplicationDocumentsDirectory();
-    String path = join(directory.path,"userdb.db");   // join append '/'
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, "userdb.db"); // join append '/'
     var myDb = await openDatabase(path, version: 1, onCreate: _onDbCreate);
     return myDb;
   }
 
-  void _onDbCreate(Database db, int version) async{
+  void _onDbCreate(Database db, int version) async {
     await db.execute(
-      "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY, $columnUsername TEXT, $columnPassword TEXT)"
+        "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY, $columnUsername TEXT, $columnPassword TEXT)"
     );
   }
 
   // CRUD operations
-  // 1- insertion
-  Future<int> saveUser(User user) async{
+
+  Future<int> saveUser(User user) async {
     var dbClient = await db;
     int res = await dbClient.insert(tableUser, user.toMap());
     return res;
   }
 
+  Future<List> getAllUser() async {
+    var dbClient = await db;
+    var res = await dbClient.rawQuery("select * from $tableUser");
+    return res.toList();
+  }
+
+  Future<int> getCount() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(
+        await dbClient.rawQuery("select count(*) from $tableUser")
+    );
+  }
+
+  Future<User> getUser(int userId) async{
+    var dbClient = await db;
+    var res = await dbClient.rawQuery(
+        "select * from $tableUser where $columnId = $userId");
+    if(res.length ==0)
+      return null;
+    else
+      return new User.fromMap(res.first);
+  }
+
+  Future<int> deleteUser(int userId) async {
+    var dbClient = await db;
+    return await dbClient.delete(tableUser,
+        where:"$columnId = ?", whereArgs: [userId]);
+  }
+
+  Future<int> updateUser(User user ) async {
+    var dbClient = await db;
+    return await dbClient.update(tableUser, user.toMap(),
+                  where: "$columnId = ?", whereArgs: [user.id]);
+  }
+
+  Future close() async {
+    var dbClient = await db;
+    return dbClient.close();
+  }
 }
--- lib/SqliteDemo.dart
+++ lib/SqliteDemo.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/DatabaseHelper.dart';
+import 'package:flutter_app/User.dart';
 import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
 
 
 class SqliteDemo extends StatefulWidget {
@@ -9,9 +13,21 @@ class SqliteDemo extends StatefulWidget {
 
 class _DbState extends State<SqliteDemo> {
 
+  var db = DatabaseHelper();
+
+  void addUser() async{
+    int res = await db.saveUser(User("mostafa","123"));
+    print(res);
+    List users = await db.getAllUser();
+    for(int i=0 ; i<users.length ; i++){
+      User user = User.map(users[i]);
+      print(user.userName);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    addUser();
     return new MaterialApp(
         home: Scaffold(
             //body:
